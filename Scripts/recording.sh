df96baf1211c7d745243abc1304d6bee2f7bd459
Pratik <pratik.neel02@gmail.com>
2024-05-10 22:21:01 +0530

    Fixed the recording script

--- Scripts/recording.sh
+++ Scripts/recording.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 pid="$(pgrep wf-recorder)"
-status="$?"
-VID="/home/pratik/Videos/Recordings/$(date +%Y-%m-%d_%H-%m-%s).mp4"
+pgrep_exit_code="$?"
 
-if [ "$status" != "0" ]; then
-  notify-send "Recording started"
-  wf-recorder -a -o eDP-1 -f "$VID"
+VID="/home/pratik/Videos/Recordings/$(date +'recording_%Y-%m-%d_%H-%m-%s').mkv"
+
+if [ "$pgrep_exit_code" != "0" ]; then
+  notify-send "Recording Started..." -i ~/.config/dunst/icons/recorder/recording.png -r 9991
+  wf-recorder -a -o eDP-1 -f "$VID" &
 else
-  notify-send "Recording saved"
+  notify-send "Recording saved" "Saved as file $VID" -i ~/.config/dunst/icons/recorder/save.png -r 9991
   pkill --signal SIGINT wf-recorder
+  while pgrep wf-recorder; do
+    sleep 0.1
+  done
 fi
 
+pkill -RTMIN+2 waybar
